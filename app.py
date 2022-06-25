c5027c7a75d96414c2c52a3e4ae59971d6edb1dd
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-25 15:13:27 +0100

    chore: remove the route `/hello/<name>` will not be used for now

--- app.py
+++ app.py
@@ -6,9 +6,4 @@ def hello():
     return 'Hello World!'
 
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{ name }}</b>!', name=name)
-
-
 run(host='localhost', port=8080, debug=True)
