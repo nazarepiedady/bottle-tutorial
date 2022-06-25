4dd740dffe3091072c709fcb46a91cb72771116a
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-25 15:05:09 +0100

    feat: create route `/hello` to return the string `hello world`

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from bottle import route, run, template
 
+
+@route('/hello')
+def hello():
+    return 'Hello World!'
+
+
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{ name }}</b>!', name=name)
