1ec7d374d78e8347b1c544bea3c1cf9b2d92562e
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-25 15:25:58 +0100

    feat: reimplement the route `/hello/<name>`, now using `@app.route`

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, run
+from bottle import Bottle, run, template
 
 
 app = Bottle()
@@ -7,6 +7,11 @@ app = Bottle()
 @app.route('/hello')
 def hello():
     return 'Hello World!'
+
+
+@app.route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}, how are you?', name=name)
 
 
 run(app, host='localhost', port=8080, debug=True)
