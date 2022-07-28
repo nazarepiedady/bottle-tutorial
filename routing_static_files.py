4db2decb61988561d37ccf0c860f12ee742d06e7
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-28 16:59:13 +0100

    feat: create the `/static/<filepath:path>` route to serve static
    * with this reoute it is possible to serve static files that are
    within the subdirectories.

--- routing_static_files.py
+++ routing_static_files.py
@@ -4,3 +4,7 @@ from bottle import static_file, route
 @route('/static/<filename>')
 def server_static(filename):
 	return static_file(filename, root='/path/to/your/static/files')
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+	return static_file(filepath, root='/path/to/your/static/files')
