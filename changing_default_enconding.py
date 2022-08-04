7dc429ba198a2317f9b048f9063047e9b0eae947
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-04 17:46:59 +0100

    feat: create and implement the route to `/latin9`

--- changing_default_enconding.py
+++ changing_default_enconding.py
@@ -7,5 +7,10 @@ def get_iso():
 	response['charset'] = 'ISO-8859-15'
 	return u'This will be sent with ISO-8859-15 enconding.'
 
+@route('/latin9')
+def get_latin():
+	response.content_type = 'text/html; charset=latin9'
+	return u'ISO-8859-15 is also known as latin9.'
+
 
 run(host='localhost', port=8080, debug=True)
