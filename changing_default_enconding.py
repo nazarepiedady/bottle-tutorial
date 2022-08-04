9e2e69652f4ff7aa38d003bf9d935839adfd09ba
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-04 17:48:31 +0100

    chore: add a note about `500 http error` caused by `response.charset` assignments

--- changing_default_enconding.py
+++ changing_default_enconding.py
@@ -3,7 +3,7 @@ from bottle import run, route, response
 
 @route('/iso')
 def get_iso():
-	#response.charset = 'ISO-8859-15'
+	#response.charset = 'ISO-8859-15' (it causes internal server error)
 	response['charset'] = 'ISO-8859-15'
 	return u'This will be sent with ISO-8859-15 enconding.'
 
