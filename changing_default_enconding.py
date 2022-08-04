244c3dc101d7d62a1852903be75cc6195263a7d4
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-04 17:42:58 +0100

    fix: use the dicionary notation to encode `/iso` to `iso-8859-15`

--- changing_default_enconding.py
+++ changing_default_enconding.py
@@ -3,7 +3,8 @@ from bottle import run, route, response
 
 @route('/iso')
 def get_iso():
-	response.charset = 'ISO-8859-15'
+	#response.charset = 'ISO-8859-15'
+	response['charset'] = 'ISO-8859-15'
 	return u'This will be sent with ISO-8859-15 enconding.'
 
 
