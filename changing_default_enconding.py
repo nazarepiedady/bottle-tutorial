89721ba196a98680f5107d8563a6a96fc054d792
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-04 17:28:31 +0100

    feat: implement `/iso` route to return an iso encoded content

---
+++ changing_default_enconding.py
@@ -0,0 +1,10 @@
+from bottle import run, route, response
+
+
+@route('/iso')
+def get_iso():
+	response.charset = 'ISO-8859-15'
+	return u'This will be sent with ISO-8859-15 enconding.'
+
+
+run(host=localhost, port=8080, debug=True)
