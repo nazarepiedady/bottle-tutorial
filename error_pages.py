a436884e6055d9df4a1031fbba84831a6f9bfa96
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-04 17:13:25 +0100

    feat: create and implement the `not found` page

---
+++ error_pages.py
@@ -0,0 +1,25 @@
+from bottle import error, run, route
+
+
+@error(404)
+def not_found(error):
+	return 'Not found, try something else'
+
+@route('/')
+def home_page():
+	return '''
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width">
+			<title>Home Page</title>
+		</head>
+		<body>
+			<h1>Welcome to Home Page</h1>
+		</body>
+	</html>
+	'''
+
+
+run(host='localhost', port=8080, debug=True)
